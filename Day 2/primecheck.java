0cc53757b1042905ac4fdb1b744914e5fbb98d9c
janeglynnis <janeglynnis@gmail.com>
2026-02-08 21:38:40 +0530

    Create primecheck.java

---
+++ Day 2/primecheck.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class PrimeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean prime = n > 1;
+        for (int i = 2; i <= Math.sqrt(n) && prime; i++)
+            if (n % i == 0) prime = false;
+        System.out.println(prime ? "Prime" : "Not Prime");
+        sc.close();
+    }
+}
