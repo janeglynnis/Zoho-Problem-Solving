5f26c4e3eeadc3600b1992a14247a0d38d880e5c
janeglynnis <janeglynnis@gmail.com>
2026-02-12 17:54:58 +0530

    Create maximumofnumber.java

---
+++ Day 4/maximumofnumber.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class Maximumnumber{
+   static int biggestOfThree(int x, int y, int z)
+    { if (x >= y && x >= z)
+            return x;
+        else if (y >= x && y >= z)
+            return y;
+        else
+            return z;
+    }
+     public static void main(String[] args){
+        int a, b, c, largest;
+        a = 5;
+        b = 10;
+        c = 3;
+        largest = biggestOfThree(a, b, c);
+        System.out.println(largest  + " is the largest number.");
+    }
+}
