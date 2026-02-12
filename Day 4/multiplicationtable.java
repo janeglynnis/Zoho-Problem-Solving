e55488e79f168e437c94cf3452fcd4ae849be03f
janeglynnis <janeglynnis@gmail.com>
2026-02-12 17:47:02 +0530

    Create multiplicationtable.java

---
+++ Day 4/multiplicationtable.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class MethodFunction{
+    public static void main(String[] args){
+      int N = 7;
+      for(int i = 0; i <= 10; i++){
+        System.out.println(N + "*" + i + "=" + N*i);
+      }
+    }
+}
