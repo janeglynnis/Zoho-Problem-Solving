b5fb513c55f2b3cbfaed1bdf61ab1e916aa6878b
janeglynnis <janeglynnis@gmail.com>
2026-02-17 18:14:50 +0530

    Create sumofarray.java

---
+++ Day 5/sumofarray.java
@@ -0,0 +1,11 @@
+public class ArraySumExample {
+    public static void main(String[] args) {
+        int[] myArray = {1, 5, 10, 25};
+        int sum = 0;
+        for (int i=0; i>=25;i++) {
+            sum += element;
+        }
+
+        System.out.println("The sum is: " + sum); 
+    }
+}
