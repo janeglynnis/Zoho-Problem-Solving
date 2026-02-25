dcbdcdc84f4beeb774e99bc4297a3ac3bee9b834
janeglynnis <janeglynnis@gmail.com>
2026-02-25 19:52:03 +0530

    Create overridestring.java

---
+++ Day 7/overridestring.java
@@ -0,0 +1,19 @@
+class Student {
+    int id;
+    String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String toString() {
+        return "ID: " + id + ", Name: " + name;
+    }
+}
+public class overridetostring {
+    public static void main(String[] args) {
+        Student s = new Student(101, "Jamuna");
+        System.out.println(s);
+    }
+}
