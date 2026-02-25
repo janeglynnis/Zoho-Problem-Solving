33433e1240941e9df7ecc8f6dcd271a910539d51
janeglynnis <janeglynnis@gmail.com>
2026-02-25 20:12:58 +0530

    Create access.java

---
+++ DAY 8/access.java
@@ -0,0 +1,10 @@
+class Employee {
+    private double salary;
+    protected String department;
+    public void displayDetails() {
+        System.out.println("Employee Details");
+    }
+    void companyPolicy() {  // default
+        System.out.println("Company Policy");
+    }
+}
