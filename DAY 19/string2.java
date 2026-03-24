be5c17294ed4c6ef43cf2035ea1e25d48922d548
janeglynnis <janeglynnis@gmail.com>
2026-03-24 19:36:53 +0530

    Create string2.java

---
+++ DAY 19/string2.java
@@ -0,0 +1,9 @@
+public class StringToInteger {
+    public static void main(String[] args) {
+        String str = "123";
+
+        int num = Integer.parseInt(str);
+
+        System.out.println("Converted Integer: " + num);
+    }
+}
