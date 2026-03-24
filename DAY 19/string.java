057c5d8efa2f4708a32eacbe4276c07edc1425d3
janeglynnis <janeglynnis@gmail.com>
2026-03-24 19:35:54 +0530

    Create string.java

---
+++ DAY 19/string.java
@@ -0,0 +1,9 @@
+
+public class StringToInteger2 {
+    public static void main(String[] args) {
+        String str = "123";
+        int num = Integer.valueOf(str);
+
+        System.out.println(num);
+    }
+}
